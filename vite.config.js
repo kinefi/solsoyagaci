e98e5d8ffd0b39f15c1777981d5ae2b6297d0e29
T. E. Kalaycı <tekrei@gmail.com>
2026-08-02 16:16:30 +0200

    Add github pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- Tailwind v4 eklentisi
+    tailwindcss(),
   ],
-  base: './', // GitHub Pages için
+  base: './solsoyagaci',
 })
